f344a060fe6b5060466a5dec2dbb34e37731a46a
Andrew Pegg <54915639+andrewcomputsci2019@users.noreply.github.com>
2023-11-28 08:25:20 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -5,10 +5,30 @@ rm -rf grading-area
 
 mkdir grading-area
 
-git clone $1 student-submission
+git clone $1 student-submission &> /dev/null
 echo 'Finished cloning'
 
-
+if ! [ -f "student-submission/ListExamples.java" ]; then
+    echo "ListExamples.java file missing make sure if the file can be found"
+    exit 1
+fi
+cp TestListExamples.java grading-area/
+cp student-submission/ListExamples.java grading-area/
+cp -r lib grading-area/
+cd grading-area
+output=`javac -cp $CPATH *.java`
+if ! [ ${?} -eq 0 ]; then
+    echo "java compliation failed: $output"
+    exit 1
+fi
+output=`java -cp $CPATH org.junit.runner.JUnitCore TestListExamples`
+if ! [ ${?} -eq 0 ]; then
+    grepOutput=`echo "$output" | grep -E -A 2 "[0-9]{1}\)[' ']?[a-z | A-Z |0-9]*['(']{1}[a-z|A-Z|0-9]*\)"`
+    numberOfTestFailed=`echo $grepOutput | wc -l`
+    echo "$numberOfTestFailed test('s') failed: $grepOutput"
+else
+    echo "All Test Passed"
+fi
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
